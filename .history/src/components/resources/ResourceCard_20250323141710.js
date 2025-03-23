bbb2e91d45ed6cecd79403a7dbab825a932863dd
vajjipartisatwikraj <satwikrajv@gmail.com>
2025-03-23 23:35:31 +0530

    Third Commit

---
+++ .history/src/components/resources/ResourceCard_20250323141710.js
@@ -0,0 +1,31 @@
+import "./ResourceCard.css";
+
+function ResourceCard(props) {
+  const imageUrl = `url(${props.image})`;
+  return (
+    <div className="ResourceCard">
+      <article className="">
+        <div className="thumb" >
+          <div className="rs-image" style={{ backgroundImage: imageUrl }}></div>
+          <h2 className="rs-inner-title">{props.title}</h2>
+        </div>
+        <div className="infos">
+          <div>
+            <h2 className="infos-title">{props.title}</h2>
+            <p className="txt">{props.desc}</p>
+          </div>
+          <div className="infos-links">
+            <a href={props.vlink} className="infos-link">
+              Videos
+            </a>
+            <a href={props.nlink} className="infos-link">
+              Notes
+            </a>
+          </div>
+        </div>
+      </article>
+    </div>
+  );
+}
+
+export default ResourceCard;
---
+++ .history/src/components/resources/ResourceCard_20250323142116.js
@@ -0,0 +1,33 @@
+import "./ResourceCard.css";
+
+function ResourceCard(props) {
+  const imageUrl = `url(${props.image})`;
+  return (
+    <div className="ResourceCard">
+      <article className="">
+        <div className="thumb" >
+          <div className="rs-image" style={{ backgroundImage: imageUrl }}></div>
+          <h2 className="rs-inner-title">{props.title}</h2>
+        </div>
+        <div className="infos">
+          <div>
+            <h2 className="infos-title">{props.title}</h2>
+            <p className="txt">{props.desc}</p>
+          </div>
+          <div className="infos-links">
+            <div className="vlinks-container" style={{display: 'flex', alignItems: 'center', gap: '10px' }}>
+              <a href={props.vlink} className="infos-link">
+                Videos
+              </a><div className="infos-link-arrow" style={{ height:'20px' }}></div>
+            </div>
+            <a href={props.nlink} className="infos-link">
+              Notes
+            </a>
+          </div>
+        </div>
+      </article>
+    </div>
+  );
+}
+
+export default ResourceCard;
